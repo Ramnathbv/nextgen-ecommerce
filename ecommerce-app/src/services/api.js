51f3cf722176b67864ac8277ace500bc0a66582b
prakash <prakash@pixelpacs.com>
2025-08-09 14:05:12 +0530

    test

--- ecommerce-app/src/services/api.js
+++
@@ -1,9 +0,0 @@
-export const fetchData = async (url) => {
-    try {
-        const response = await fetch(url);
-        if(!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-        return await response.json();
-    } catch(error) {
-        throw new Error(error.message || "Something Went Wrong")
-    }
-}
